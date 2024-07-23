561bd7d3ab39c001f2d8ab86b1be008df2f0d8f8
wajahat-baloch188 <wajahatbaloch2002@gmail.com>
2024-07-23 22:43:24 +0500

    second

---
+++ src/db/index.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants";
+import express from "express";
+
+const app = express();
+
+async function ConnectDB() {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n MongoDB Connected !! DB HOST:${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.error("MongoDB Connection Erorr: ", error);
+    process.exit(1);
+  }
+}
+
+export default ConnectDB;
---
+++ src/index.js
@@ -0,0 +1,29 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv";
+import ConnectDB from "./db";
+
+dotenv.config({
+  path: "./env",
+});
+
+/*
+import express from "express";
+
+const app = express();
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (err) => {
+      console.log("Error", err);
+      throw err;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`app is listening on Port, ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("ERROR: ", error);
+    throw error;
+  }
+})();
+*/
